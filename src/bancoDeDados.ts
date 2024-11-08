72fa28575ec1d83e370d213beba53be50160362d
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2024-11-08 11:39:50 -0300

    implementação do detalhamento de autores

--- src/bancoDeDados.ts
+++ src/bancoDeDados.ts
@@ -1,6 +1,11 @@
 import Autor from "./modelos/Autor";
 import Post from "./modelos/Post";
 
-export const autores:Autor[]= []
+export const autores:Autor[]= [
+   new Autor({
+    nome: 'Rafael',
+    idade: 38
+   })
+]
 export const posts:Post[]= []
 
--- src/rotas.ts
+++ src/rotas.ts
@@ -6,6 +6,7 @@ const rotas =Router()
 const autorControlador:any = new AutorControlador()
 
 rotas.get('/autores',autorControlador.listar)
+rotas.get('/autores/:id',autorControlador.listar)
 
 
 export default rotas
